b3ef2c86cf4e84fa56360a5f77de2691c2e8c34f
julia <144443718+julia-lis06@users.noreply.github.com>
2023-09-13 12:16:53 +0100

    Add files via upload

---
+++ 55.py
@@ -0,0 +1,9 @@
+
+FirstN = input("Please insert First name ")
+print("Hello",FirstN)
+SecondN = input("Please insert Second name ")
+print("Hello",FirstN,SecondN)
+
+print("“What do you call a bear with no teeth?”-Chloe")
+input()
+print(" “A gummy bear!”-Chloe")
