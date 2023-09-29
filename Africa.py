5c30699a4ee3a03b92ad72d3d42cc0aec876df05
julia <144443718+julia-lis06@users.noreply.github.com>
2023-09-29 12:28:56 +0100

    Add files via upload

---
+++ Africa.py
@@ -0,0 +1,14 @@
+countries = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Cameroon", "Central African Republic", "Chad", "Comoros", "Ivory Coast", "Djibouti", "Democratic Republic of the Congo", "Egypt", "Equatorial Guinea", "Eritrea", "Eswatini", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Republic of the Congo", "Rwanda", "Sao Tome &Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "South Africa", "South Sudan", "Sudan", "Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"]
+print("--countries of africa quiz--")
+score=0
+while len(countries)>0:
+    print("Number of Countries to guess:")
+    country = input("Enter the name of a country:54")
+    if country in countries:
+            print("Good Guess")
+            score = score + 1
+             countries.remove(country)
+    else :
+            print("Invalid guess")
+    print(score)
+
