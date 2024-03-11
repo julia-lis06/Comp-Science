310b217e3303f02746b447a5123a089763fac180
julia <144443718+julia-lis06@users.noreply.github.com>
2024-03-11 11:23:19 +0000

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,13 @@
+import meow
+
+inp1 = int(input("Please enter a number..."))
+inp2 = int(input("Please Enter another number..."))
+result = meow.sploosh(inp1,inp2)
+print(result)
+
+word = input("Please enter a word...")
+total = meow.plop(word)
+print(total)
+
+pali = input("Enter a Word, I'll tell you whether its a palindrome")
+
---
+++ meow.py
@@ -0,0 +1,19 @@
+def sploosh (a,b):
+    total = a + b
+    return total
+
+def plop(a):
+    wordword = "un" + a
+    return wordword
+
+def shlop(a):
+    f = list(a)
+    g = list(a)
+    f.reverse()
+    if f == g:
+        return true
+    else:
+        return false
+    
+    
+
